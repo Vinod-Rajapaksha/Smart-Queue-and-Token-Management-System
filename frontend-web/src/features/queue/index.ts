88947ab51ea0f2c74e7d210e32ab7231b729a5da
Vinod-Rajapaksha <165497570+Vinod-Rajapaksha@users.noreply.github.com>
2026-01-08 17:54:22 +0530

    feat(queue): add live and list queue pages with actions and barrel exports

---
+++ frontend-web/src/features/queue/index.ts
@@ -0,0 +1,15 @@
+// types
+export * from "./types";
+
+// api
+export * from "./api/queue.api";
+export * from "./api/queue.endpoints";
+
+// pages
+export { default as QueueListPage } from "./pages/QueueListPage";
+export { default as QueueLivePage } from "./pages/QueueLivePage";
+
+// components
+export { default as QueueCard } from "./components/QueueCard";
+export { default as QueueActions } from "./components/QueueActions";
+export { default as QueueStatusBadge } from "./components/QueueStatusBadge";
