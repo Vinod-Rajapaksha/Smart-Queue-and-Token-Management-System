cd2c17fbcda2de7ab5d309fa200d74cd2a500748
Vinod-Rajapaksha <165497570+Vinod-Rajapaksha@users.noreply.github.com>
2026-02-03 00:46:03 +0530

    feat(app): wrap root layout with react-native-paper provider

--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -4,14 +4,17 @@ import { Provider } from "react-redux";
 import { store } from "../src/store/store";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider as PaperProvider } from "react-native-paper";
 
 export default function RootLayout() {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <StatusBar style="auto" />
-        <Stack screenOptions={{ headerShown: false }} />
-      </SafeAreaProvider>
+      <PaperProvider>
+        <SafeAreaProvider>
+          <StatusBar style="auto" />
+          <Stack screenOptions={{ headerShown: false }} />
+        </SafeAreaProvider>
+      </PaperProvider>
     </Provider>
   );
 }
